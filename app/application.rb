a645303c3502ed57fa343e5de226695d11057970
josh-sea <joshua.cocciardi@gmail.com>
2018-12-09 23:00:34 -0500

    Done.

--- app/application.rb
+++ app/application.rb
@@ -7,7 +7,7 @@ class Application
 
 
    if req.path.match(/items/)
-      item_name = req.path.split("/items/").last #turn /songs/item_name as item name
+      item_name = req.path.split("/items/").last #turn /items/item_name as item name
       item_inst  = @@items.find{|item| item.name == item_name}
       if item_inst == nil
         resp.write "Item not found"
